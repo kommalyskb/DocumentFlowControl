73db0c1897f6a6136885fb017371c11c1d5e35f8
kommalyskb <kommalyskb@users.noreply.github.com>
2023-02-15 23:47:50 +0700

    Change render file method

--- DFM.Frontend/Pages/UserComponent/UserView.razor.cs
+++ DFM.Frontend/Pages/UserComponent/UserView.razor.cs
@@ -21,11 +21,11 @@ namespace DFM.Frontend.Pages.UserComponent
         private async Task previewImageProfile(string bucket, string fileName)
         {
             // get file obj
-            var obj = await minio.GetObject(bucket, fileName);
+            var obj = await minio.GenerateLink(bucket, fileName);
             if (obj.IsSuccess)
             {
                 //imageUri = link.Response.Replace(endpoint.MinioAPI, endpoint.StorageAPI);
-                imageUri = $"data:image/png;base64,{obj.ByteStream.ToBase64()}";
+                imageUri = obj.Response;//$"data:image/png;base64,{obj.ByteStream.ToBase64()}";
             }
 
         }
